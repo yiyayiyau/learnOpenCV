7ce8c4789ff33fda7f7632baa37cb2f4354f92f1
yiyayiyau <33147109+yiyayiyau@users.noreply.github.com>
2020-02-21 14:07:04 +0100

    Update distance.py

--- distance.py
+++ distance.py
@@ -10,22 +10,22 @@ Fréchet distance
 """
 import math
 import numpy as np
-
+class dist():
 # Euclidean distance.
-def euc_dist(pt1,pt2):
+def euc(pt1,pt2):
     return math.sqrt((pt2[0]-pt1[0])*(pt2[0]-pt1[0])+(pt2[1]-pt1[1])*(pt2[1]-pt1[1]))
 
 def _c(ca,i,j,P,Q):
     if ca[i,j] > -1:
         return ca[i,j]
     elif i == 0 and j == 0:
-        ca[i,j] = euc_dist(P[0],Q[0])
+        ca[i,j] = euc(P[0],Q[0])
     elif i > 0 and j == 0:
-        ca[i,j] = max(_c(ca,i-1,0,P,Q),euc_dist(P[i],Q[0]))
+        ca[i,j] = max(_c(ca,i-1,0,P,Q),euc(P[i],Q[0]))
     elif i == 0 and j > 0:
-        ca[i,j] = max(_c(ca,0,j-1,P,Q),euc_dist(P[0],Q[j]))
+        ca[i,j] = max(_c(ca,0,j-1,P,Q),euc(P[0],Q[j]))
     elif i > 0 and j > 0:
-        ca[i,j] = max(min(_c(ca,i-1,j,P,Q),_c(ca,i-1,j-1,P,Q),_c(ca,i,j-1,P,Q)),euc_dist(P[i],Q[j]))
+        ca[i,j] = max(min(_c(ca,i-1,j,P,Q),_c(ca,i-1,j-1,P,Q),_c(ca,i,j-1,P,Q)),euc(P[i],Q[j]))
     else:
         ca[i,j] = float("inf")
     return ca[i,j]
