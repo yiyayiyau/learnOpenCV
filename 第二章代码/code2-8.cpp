33166e550208c752c070857e767904c49bbc43bd
yiyayiyau <33147109+yiyayiyau@users.noreply.github.com>
2020-03-18 17:50:40 +0100

    两次图像金字塔,即收缩图像后,使用Canny边缘检测

--- 第二章代码/code2-8.cpp
+++ 第二章代码/code2-8.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char** argv){
     cv::Mat img_rgb, img_gry, img_cny;
+    cv::Mat img_pyr, img_pyr2;
     
     cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
     cv::namedWindow("Example2", cv::WINDOW_AUTOSIZE);
@@ -10,9 +11,11 @@ int main(int argc, char** argv){
     img = cv::imread( argv[1] );
     
     cv::cvtColor(img_rgb, img_gry, cv::COLOR_BGR2GRAY);
+    cv::pyrDown( img_gry, img_pyr );
+    cv::pyrDown( img_pyr, img_pyr2 );
     cv::imshow("Example1", img_gry);
     
-    cv::Canny( img_gry, img_cny, 10, 100, 3, true);
+    cv::Canny( img_pyr2, img_cny, 10, 100, 3, true);
     cv::imshow("Example2", img_cny);
     
     cv::waitKey(0);
