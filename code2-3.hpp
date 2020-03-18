eef0f6072e57b9d26721149542b1cba3b05b47b4
yiyayiyau <33147109+yiyayiyau@users.noreply.github.com>
2020-03-18 15:37:29 +0100

    Create code2-3.hpp

---
+++ code2-3.hpp
@@ -0,0 +1,18 @@
+// open a film
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+int main(int argc, char** argv){
+    cv::namedWindow("Example3", cv::WINDOW_AUTOSIZE);
+    cv::VideoCapture cap;
+    cap.open( string(argv[1]) );
+    cv::Mat frame;
+    for(;;){
+        cap >> frame;
+        if (frame.empty()) break;
+        cv::imshow("Example3", frame);
+        if ( cv::waitKey(33) >= 0 ) break; //wait for 33 ms (show one frame 33 ms, 33 FPS)
+    }
+    return 0;
+}
