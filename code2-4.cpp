fe46d1049b12776670ada91212070fce2e2e22dd
yiyayiyau <33147109+yiyayiyau@users.noreply.github.com>
2020-03-18 16:48:12 +0100

    Update code2-4.cpp

--- code2-4.cpp
+++ code2-4.cpp
@@ -1,21 +1,25 @@
 // open a film with trackbar
-
+// g_run = 1 单步模式, 只读取一张图片
+// g_run = -1, -2,... 连续视频模式
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <fstream>
 
 using namespace std;
-
+// 添加全局变量来表示滑动条的位置,并且添加一个回调函数来更改这个变量来重新定位读取的位置
 int g_slider_position = 0;
-int g_run = 1, g_dontset =0; //start out in single step mode 以单步模式开始
+int g_run = 1; //start out in single step mode 以单步模式开始
+int g_dontset = 0; //避免在调整进度条的时候触发单步模式
 cv::VideoCapture g_cap;
 
 void onTrackbarSlide( int pos, void *){
-    g_cap.set(cv::CAP_PROP_POS_FRAMES, pos);
+    // 当视频前进的时候,滑动条也跟着前进, 并且不会触发单步模式
+    // 调整滑动条位置
+    g_cap.set(cv::CAP_PROP_POS_FRAMES, pos); 
     
-    if(!g_dontset)
-        g_run = 1;
+    if(!g_dontset)// 一旦触发滑动条事件,该设置生效,避免触发单步模式
+        g_run = 1;// 跳转之后, g_run = 1, 跑一步之后, g_run = 0, 停止等待用户输入模式
     g_dontset = 0;
 }
 
@@ -27,7 +31,7 @@ int main(int argc, char** argv){
     int tmpw = (int) g_cap.get(cv::CAP_PROP_FRAME_WIDTH);
     int tmph = (int) g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
     cout << "Video has " << frames <<" frames of dimensions (" << temw << " , " << temh << ")." <<endl;
-    
+    // 创建一个滑动条,指明窗口,设置总frame数,以及在滑动条移动的时候的回调函数
     cv::createTrackbar("Position", "Example2_4", &g_slider_position, frames, onTrackbarSlider);
     cv::Mat frame;
     for(;;){
@@ -40,7 +44,7 @@ int main(int argc, char** argv){
             cv::setTrackbarPos("Position", "Example2_4", current_pos);
             cv::imshow("Example2_4", frame);
             
-            g_run = -1;
+            g_run -= 1; 
             
         }
         char C = (char) cv::waitKey(10);
